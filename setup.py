3d6d019ab904e7bbe839d4e9bfb16533d1b82fad
cfblaeb <laeb@biosustain.dtu.dk>
2018-12-17 14:22:35 +0100

    include readme on pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="metmap",
-    version="1.0.3",
+    version="1.0.4",
     author="Lasse Ebdrup Pedersen",
     author_email="laeb@biosustain.dtu.dk",
     description="A tool for generating DNA MTase motif testing sequences",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     scripts=['bin/run_metmap.py', ],
-    url="https://github.com/biosustain/metmap",
+    url="https://github.com/cfblaeb/metmap",
     packages=['metmap'],
     install_requires=['biopython'],
     python_requires='>=3.6',
